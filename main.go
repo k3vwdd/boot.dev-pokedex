4346aef6a2b14d020d473fc25ddd2715b8ac1690
k3vwdd <kevwdiaz@outlook.com>
2025-02-15 17:47:20 -0600

    feature: Basic repl created

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -9,6 +12,20 @@ func cleanInput(text string) []string {
     return strings.Fields(text)
 }
 
-
 func main() {
+    fmt.Print("Pokedex > ")
+    userInput := bufio.NewScanner(os.Stdin)
+    for userInput.Scan() {
+        words := strings.Fields(strings.ToLower(userInput.Text()))
+        var command string
+        if len(words) > 0 {
+            command = words[0]
+            fmt.Print("Your command was: ", command)
+            continue
+        }
+        fmt.Print("Pokedex > ")
+    }
+    if err := userInput.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "error:", err)
+    }
 }
